8630e0caad23e7237a8f0619ac8e81796acc6ee1
Javi Roman <javiroman@kernel-labs.org>
2012-09-21 12:17:37 +0200

    JavaClient well indented

--- cmpi-providers-libs/sblim-cim-client-java/src/JavaClient.java
+++ cmpi-providers-libs/sblim-cim-client-java/src/JavaClient.java
@@ -17,57 +17,58 @@ import javax.cim.CIMInstance;
 
 class JavaClient
 {  
-public static void main(String args[])
-		{
-				System.out.println("CIM Java client");
+    public static void main(String args[])
+    {
+        System.out.println("CIM Java client");
 
-				try {
-						WBEMClient cli = WBEMClientFactory.getClient("CIM-XML");
-						Subject subject = new Subject();
-						//subject.getPrincipals().add(new UserPrincipal(""));
-						//subject.getPrivateCredentials().add(new PasswordCredential(""));
-						//PasswordCredential password = new PasswordCredential("userPassword");
-						CIMObjectPath cop = new CIMObjectPath("https", 
-															"localhost", 
-															"5989", 
-															"/root/cimv2",
-															null,
-															null);
+        try {
+		WBEMClient cli = WBEMClientFactory.getClient("CIM-XML");
+		Subject subject = new Subject();
+		//subject.getPrincipals().add(new UserPrincipal(""));
+		//subject.getPrivateCredentials().add(new PasswordCredential(""));
+		//PasswordCredential password = new PasswordCredential("userPassword");
+		CIMObjectPath cop = new CIMObjectPath("https", 
+                                    "localhost", 
+                                    "5989", 
+                                    "/root/cimv2",
+                                    null,
+                                    null);
 
-						System.out.println(cop.getHost()); 
-						System.out.println(cop.getNamespace()); 
-						System.out.println(cop.getPort()); 
+		System.out.println(cop.getHost()); 
+		System.out.println(cop.getNamespace()); 
+		System.out.println(cop.getPort()); 
 
-						cli.initialize(cop, subject, null);
+		cli.initialize(cop, subject, null);
 
-						CloseableIterator itr = cli.execQuery(cop, "select * from KC_Widget", "WQL");
-						while(itr.hasNext()) {
+		CloseableIterator itr = cli.execQuery(cop, "select * from KC_Widget", "WQL");
+		while(itr.hasNext()) {
+			Object element = itr.next(); 
+			System.out.print(element + "\n");
+		} 
 
-    						Object element = itr.next(); 
-    						System.out.print(element + "\n");
-						} 
+		CIMArgument<?>[] input = new CIMArgument[2];
+		CIMArgument<?>[] output = new CIMArgument[2];
+		/*
+		   input[0] = new CIMArgument("X", new CIMDataType(UINT32), 1);
+		   input[1] = new CIMArgument("Y", CIMDataType.UINT32, 1);
 
-						CIMArgument<?>[] input = new CIMArgument[2];
-						CIMArgument<?>[] output = new CIMArgument[2];
-/*
-						input[0] = new CIMArgument("X", new CIMDataType(UINT32), 1);
-						input[1] = new CIMArgument("Y", CIMDataType.UINT32, 1);
+		   Object obj = 
+		   cli.invokeMethod(new CIMObjectPath("/root/cimv2:KC_Widget"), 
+		   "Add", input, output);
 
-						Object obj = 
-								cli.invokeMethod(new CIMObjectPath("/root/cimv2:KC_Widget"), 
-												"Add", input, output);
-
-						if (obj.toString().equals("0")) 
-						{
-								System.out.println("Indication generated successfully...");
-						}
+		   if (obj.toString().equals("0")) {
+		   	System.out.println("Indication generated successfully...");
+		   }
 
 */
-				} catch (WBEMException e) {
-						// Print out the exception that occurred
-						System.out.println(e);
+	} catch (WBEMException e) {
+		// Print out the exception that occurred
+		System.out.println(e);
 
-				}
-		}
+	}
+    }
 }
+
+
+
 

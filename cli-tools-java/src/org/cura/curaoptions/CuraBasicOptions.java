d712d7d951708cd119ce091fa8769335704f80b1
Javi Roman <javiroman@kernel-labs.org>
2012-09-24 17:47:24 +0200

    package hierarchy doesn't work properly

---
+++ cli-tools-java/src/org/cura/curaoptions/CuraBasicOptions.java
@@ -0,0 +1,44 @@
+package org.cura.curaoptions;
+
+public class CuraBasicOptions
+{
+	public CuraBasicOptions() {
+		System.out.println("CIM Java client");	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 et sw=4 tw=0 sts=4: */

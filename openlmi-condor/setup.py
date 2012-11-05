494a57e84601951a63f046ff1a219fcd0b74a8e8
Javi Roman <javiroman@kernel-labs.org>
2012-11-05 10:47:24 +0100

    fixed setup file

--- openlmi-condor/setup.py
+++ openlmi-condor/setup.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 setup(
     name='openlmi-condor',
+    version='0.1',
     description='Condor Batch Scheduler Provider',
     author='Javi Roman',
     author_email='javiroman@kernel-labs.org',
     url='https://github.com/javiroman/openlmi-poc/tree/master/openlmi-condor',
-    version='0.1',
-    package_dir={'openlmi.storage': 'providers'},
-    packages=['openlmi.storage', 'openlmi.storage.wrapper', 'openlmi.storage.util'],
+    package_dir={'openlmi.condor':'providers'},
+    packages=['openlmi.condor'],
     classifiers=[
+	'Development Status :: 1 - Alpha',
+	'Environment :: Console',
+	'Environment :: Web WBEM Environment',
         'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
         'Operating System :: POSIX :: Linux',
         'Topic :: System :: Systems Administration',
-        ]
-    )
+	'Programming Language :: Python',
+	'Intended Audience :: Developers',
+	'Intended Audience :: System Administrators',
+       ]
+)
